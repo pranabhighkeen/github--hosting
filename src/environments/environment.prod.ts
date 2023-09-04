b4c28c4fda882457656e04a41d66c4f67dfe4554
pranabhighkeen <pranab.dey@highkeen.com>
2023-09-04 15:23:10 +0530

    project init

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,26 @@
+export const environment={
+    production:true,
+    firebase:{
+        apiKey: "AIzaSyCzE__uFFOehn041uQ16tBYKzs8X_SYs3Y",
+    
+        authDomain: "pushnotificationspringbo-34917.firebaseapp.com",
+      
+        projectId: "pushnotificationspringbo-34917",
+      
+        storageBucket: "pushnotificationspringbo-34917.appspot.com",
+      
+        messagingSenderId: "49272912924",
+      
+        appId: "1:49272912924:web:a94e51b9c3c3664dd033d9",
+      
+        measurementId: "G-06KWK7NBHY",
+
+        vapidKey: "BPxucQbBhVStY42n1HCEBWqQugcg3LUMKcTbNsjdaKv3IZQOS5zOP7XrLZTgzN5LtgNyD5kXwYdpepWUNbxz9yI"
+    
+    
+    
+      }
+    
+    
+    
+    }
---
+++ src/firebase-messaging-sw.js
@@ -0,0 +1,18 @@
+importScripts("https://www.gstatic.com/firebasejs/9.1.3/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.1.3/firebase-messaging-compat.js");
+firebase.initializeApp({
+    apiKey: "AIzaSyCzE__uFFOehn041uQ16tBYKzs8X_SYs3Y",
+
+    authDomain: "pushnotificationspringbo-34917.firebaseapp.com",
+  
+    projectId: "pushnotificationspringbo-34917",
+  
+    storageBucket: "pushnotificationspringbo-34917.appspot.com",
+  
+    messagingSenderId: "49272912924",
+  
+    appId: "1:49272912924:web:a94e51b9c3c3664dd033d9",
+  
+    measurementId: "G-06KWK7NBHY"
+});
+const messaging = firebase.messaging();
